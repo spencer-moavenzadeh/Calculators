794a766c6b8259cface28a98d32b36315e3ccebe
Spencer Moavenznadeh <spencer.moavenzadeh@duke.edu>
2019-09-09 12:40:05 -0400

    Added cholesterol interface and check HDL function to check HDL cholesterol level

--- calculator.py
+++ calculator.py
@@ -7,10 +7,19 @@ def check_HDL(HDL_result):
         return "Borderline Low"
     else: 
         return "Low"
+        
+def cholesterol_interface():
+    print("Checking Cholesterol Results:")
+    cholInput = input("Enter your cholesterol test results (in format 'test=result' with test options HDL or LDL): ")
+    cholData = cholInput.split("=")
+    if cholData[0] == "HDL":
+        print("Your HDL result is {}".format(cholData[1]))
+        print(check_HDL(int(cholData[1])))
 
 def interface():
     print("Caluculator Program:")
     print("Options Menu:")
+    print("Cholesterol Check = 1")
     print("Quit = 9")
     menuFlag = True
     
@@ -19,6 +28,9 @@ def interface():
         if menuChoice == '9':
             menuFlag = False
             return
+        elif menuChoice == '1':
+            menuFlag = False
+            cholesterol_interface()
         else:
             print("Input is not an option. Try again.")
         
