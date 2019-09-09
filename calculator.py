fb43312a89a1939306181e60ab03dfccc2251e63
Spencer Moavenznadeh <spencer.moavenzadeh@duke.edu>
2019-09-09 12:23:35 -0400

    Updated calculator.py to include HDL check function

--- calculator.py
+++ calculator.py
@@ -1,5 +1,13 @@
 # Calculator file
 
+def check_HDL(HDL_result):
+    if HDL_result >= 60:
+        return "Normal"
+    elif 40 <= HDL_result < 60:
+        return "Borderline Low"
+    else: 
+        return "Low"
+
 def interface():
     print("Caluculator Program:")
     print("Options Menu:")
