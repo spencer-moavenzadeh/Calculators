d860edbe4c22620e8329abf351f92ecc90fd19d3
Spencer Moavenznadeh <spencer.moavenzadeh@duke.edu>
2019-09-09 12:08:48 -0400

    Added loop to interface to validate user input

--- calculator.py
+++ calculator.py
@@ -4,10 +4,15 @@ def interface():
     print("Caluculator Program:")
     print("Options Menu:")
     print("Quit = 9")
-    menuChoice = input("Enter option: ")
+    menuFlag = True
     
-    if menuChoice == '9':
-        return
+    while menuFlag:
+        menuChoice = input("Enter menu option: ")
+        if menuChoice == '9':
+            menuFlag = False
+            return
+        else:
+            print("Input is not an option. Try again.")
         
 if __name__ == "__main__":
     interface()
