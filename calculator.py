3d5813bc234c9a005f7ef0cc1763338e962e07e6
Spencer Moavenznadeh <spencer.moavenzadeh@duke.edu>
2019-09-09 12:05:03 -0400

    Creating template user interface for calculator.py

---
+++ calculator.py
@@ -0,0 +1,14 @@
+# Calculator file
+
+def interface():
+    print("Caluculator Program:")
+    print("Options Menu:")
+    print("Quit = 9")
+    menuChoice = input("Enter option: ")
+    
+    if menuChoice == '9':
+        return
+        
+if __name__ == "__main__":
+    interface()
+    
